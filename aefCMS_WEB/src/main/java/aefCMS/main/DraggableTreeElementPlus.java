fb824b6a88395394a0d39dcb7d90879f51da19a7
aalfodel <aalfodel@OPENGATE>
2018-08-03 18:10:32 +0200

    Added "draggable movement"

--- aefCMS_WEB/src/main/java/aefCMS/main/DraggableTreeElementPlus.java
+++ aefCMS_WEB/src/main/java/aefCMS/main/DraggableTreeElementPlus.java
@@ -1,14 +1,19 @@
 package aefCMS.main;
 
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+
 import biz.opengate.zkComponents.draggableTree.DraggableTreeElement;
 
 public class DraggableTreeElementPlus extends DraggableTreeElement {
 
+	private IndexVM fatherVm;
 	private PageElement pageElement;
 
-	public DraggableTreeElementPlus(DraggableTreeElement parent, String description, PageElement pageElement) {
+	public DraggableTreeElementPlus(DraggableTreeElement parent, String description, PageElement pageElement, IndexVM fatherVm) {	//added father to notify it of the movement
 		super(parent, description);
 		this.pageElement = pageElement;
+		this.fatherVm=fatherVm;
 	}
 
 	public PageElement getPageElement() {
@@ -22,6 +27,19 @@ public class DraggableTreeElementPlus extends DraggableTreeElement {
 		this.getPageElement().setParent(newParent.getPageElement());
 		int index = spacer.getParent().getChildren().indexOf(spacer) / 2;
 		newParent.getPageElement().getChildren().add(index, this.getPageElement());
+		
+		try {
+			notifyFatherOfElementsMovement();
+		} catch (ResourceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	protected void addTo(DraggableTreeElement newParent) {
@@ -29,7 +47,23 @@ public class DraggableTreeElementPlus extends DraggableTreeElement {
 		this.getPageElement().getParent().getChildren().remove(this.getPageElement());
 		DraggableTreeElementPlus newParentPlus = (DraggableTreeElementPlus) newParent;
 		this.getPageElement().setParent(newParentPlus.getPageElement());
-		newParentPlus.getPageElement().getChildren().add(this.getPageElement());		
+		newParentPlus.getPageElement().getChildren().add(this.getPageElement());	
+		
+		try {
+			notifyFatherOfElementsMovement();
+		} catch (ResourceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseErrorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
+	private void notifyFatherOfElementsMovement() throws ResourceNotFoundException, ParseErrorException, Exception {
+		fatherVm.moveElement();
+	}
 }
